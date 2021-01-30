3cc71d41df8c4f2d6a459f669fc793995688b2af
Katherine Trammell <ktrammell.com>
2021-01-29 21:01:57 -0500

    Created Pet Table Ran Migration

--- Models/DatabaseContext.cs
+++ Models/DatabaseContext.cs
@@ -14,8 +14,7 @@ namespace TamagotchiAPI.Models
         private static bool LOG_SQL_STATEMENTS_IN_DEVELOPMENT = false;
 
         // Add database tables here
-
-
+        public DbSet<Pet> Pets { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             if (LOG_SQL_STATEMENTS_IN_DEVELOPMENT && Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") == "Development")
